2596fa1d5b9d1d7825c499f842ce805cbbd79ea5
Yana <heytokky@gmail.com>
2025-07-22 19:16:58 +0200

    beta version of the main page (DST) and NDST page, adaptation for mobile

--- RhymesOfLife/base/urls.py
+++ RhymesOfLife/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('ndst/', views.ndst, name='ndst'),
 ]
 
--- RhymesOfLife/base/views.py
+++ RhymesOfLife/base/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')  # Look for 'home.html' inside 'templates/base/'
+
+def ndst(request):
+       return render(request, 'ndst.html')
